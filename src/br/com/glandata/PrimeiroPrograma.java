84a0316bdcd98a53673ec79529bd786568f0ad29
Alessandra Lima <alessandra-pl@hotmail.com>
2022-05-18 20:23:02 -0300

    Alteração do Sysout

--- src/br/com/glandata/PrimeiroPrograma.java
+++ src/br/com/glandata/PrimeiroPrograma.java
@@ -3,7 +3,7 @@ package br.com.glandata;
 public class PrimeiroPrograma {
 	
 	public static void main(String[] args) {
-		System.out.println("Teste!");
+		System.out.println("Olá Glan Data");
 	}
 
 }
