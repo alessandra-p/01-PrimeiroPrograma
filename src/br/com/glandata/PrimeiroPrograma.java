f82e2894d1287487e85194d6cb59efbf46cead22
Alessandra Lima <alessandra-pl@hotmail.com>
2022-05-18 20:55:52 -0300

    Nova alteração GIT no Eclipse

--- src/br/com/glandata/PrimeiroPrograma.java
+++ src/br/com/glandata/PrimeiroPrograma.java
@@ -3,7 +3,7 @@ package br.com.glandata;
 public class PrimeiroPrograma {
 	
 	public static void main(String[] args) {
-		System.out.println("Olá Glan Data");
+		System.out.println("Outra alteração");
 	}
 
 }
---
+++ src/br/com/glandata/TesteGit.java
@@ -0,0 +1,9 @@
+package br.com.glandata;
+
+public class TesteGit {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
